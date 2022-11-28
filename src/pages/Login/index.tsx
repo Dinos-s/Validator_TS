6205d6b6ed1b2a71f7506dce787031ebe6ace394
Gabriel <gabriel02ramos@gmail.com>
2022-11-28 14:54:05 -0300

    Configs iniciais

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,22 @@
+import { 
+    Column, 
+    Container, 
+    LoginContainer, 
+    Spacing, 
+    Title 
+} from "./style"
+
+const Login = () => {
+    return(<Container>
+        <LoginContainer>
+            <Column>
+                <Title></Title>
+                <Spacing/>
+                <Spacing/>
+                <Spacing/>
+            </Column>
+        </LoginContainer>
+    </Container>)
+}
+
+export default Login
